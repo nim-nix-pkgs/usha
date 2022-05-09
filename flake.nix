7ce42027aa783c8bd5c27431a0d0585922d33a0f
hugosenari <hugosenari@gmail.com>
2022-05-09 20:45:05 -0300

    chore: re index usha

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."usha-master".dir   = "master";
-  inputs."usha-master".owner = "nim-nix-pkgs";
-  inputs."usha-master".ref   = "master";
-  inputs."usha-master".repo  = "usha";
-  inputs."usha-master".type  = "github";
-  inputs."usha-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."usha-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."usha-0_2".dir   = "0_2";
-  inputs."usha-0_2".owner = "nim-nix-pkgs";
-  inputs."usha-0_2".ref   = "master";
-  inputs."usha-0_2".repo  = "usha";
-  inputs."usha-0_2".type  = "github";
-  inputs."usha-0_2".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."usha-0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
